c0fe8d71bd9298c325220002681be2fb10a1b92d
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-11 09:28:33 +0100

    klar

---
+++ Näsan steg 1 övning 6/Program.cs
@@ -0,0 +1,18 @@
+﻿string input;
+int output;
+bool senap = false;
+
+Console.WriteLine("Skriv in en siffra, int eller vafan du vill sålänge det kan bli en integer, snälla rara barn");
+
+while(senap == false){
+    input = Console.ReadLine();
+if(int.TryParse(input, out output)){
+    Console.WriteLine("Bra jobbat, du kan skriva siffror");
+    senap = true;
+    Thread.Sleep(3000);
+    Environment.Exit(0);
+}else{
+    Console.WriteLine("Sämst.");
+}
+}
+
